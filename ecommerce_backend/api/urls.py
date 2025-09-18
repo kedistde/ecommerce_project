70bf299536afa8f97db46d291ac8c61cd2501bb4
kedistde <76200367+kedistde@users.noreply.github.com>
2025-09-18 21:40:29 +0300

    Update urls.py

--- ecommerce_backend/api/urls.py
+++ ecommerce_backend/api/urls.py
@@ -1,1 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ProductViewSet, CategoryViewSet, OrderViewSet, RegisterView, UserProfileView
 
+router = DefaultRouter()
+router.register(r'products', ProductViewSet)
+router.register(r'categories', CategoryViewSet)
+router.register(r'orders', OrderViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('auth/register/', RegisterView.as_view(), name='auth_register'),
+    path('auth/profile/', UserProfileView.as_view(), name='user_profile'),
+]
