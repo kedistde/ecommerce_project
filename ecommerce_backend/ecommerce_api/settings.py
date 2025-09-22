e3a563433a388f4b8ceb313e8675ba72c4df4095
kedistde <76200367+kedistde@users.noreply.github.com>
2025-09-22 15:10:44 +0300

    Update settings.py

--- ecommerce_backend/ecommerce_api/settings.py
+++ ecommerce_backend/ecommerce_api/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = 'django-insecure-your-secret-key-here-change-in-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['Kidy.pythonanywhere.com']
 
 # Application definition
 INSTALLED_APPS = [
