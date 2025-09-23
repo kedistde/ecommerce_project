c8d70ba1c38c58417c74818836f5ec0c360c0778
kedistde <76200367+kedistde@users.noreply.github.com>
2025-09-23 18:51:25 +0300

    Update wsgi.py

--- ecommerce_backend/ecommerce_api/wsgi.py
+++ ecommerce_backend/ecommerce_api/wsgi.py
@@ -1,7 +1,14 @@
 import os
+import sys
 
+# Add your project directory to the Python path
+path = '/home/Kidy/ecommerce_project/ecommerce_backend'
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+# Set environment variables
+os.environ['DJANGO_SETTINGS_MODULE'] = 'ecommerce_api.settings'
+
+# Import and configure Django
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce_api.settings')
-
 application = get_wsgi_application()
