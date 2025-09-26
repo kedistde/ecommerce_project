b6d037d6d011e89cb5624535ae10e52a5df860d2
kedistde <76200367+kedistde@users.noreply.github.com>
2025-09-26 17:03:54 +0300

    Update urls.py

--- ecommerce_backend/ecommerce_api/urls.py
+++ ecommerce_backend/ecommerce_api/urls.py
@@ -3,10 +3,19 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from frontend.views import FrontendView
+# from frontend.views import FrontendView
+from rest_framework.routers import DefaultRouter
+from . import views
 
+router = DefaultRouter()
+
+# Add basename parameter
+router.register('products', views.ProductViewSet, basename='product')
+router.register('categories', views.CategoryViewSet, basename='category')
+router.register('orders', views.OrderViewSet, basename='order')
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
